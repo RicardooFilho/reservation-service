f856b94d5662e2d702bf00a98fcad4e850bae137
Ricardo Tagami <ricardo-tagami@hotmail.com>
2025-06-09 20:09:50 -0300

    ajustes finais

--- service/src/main/java/com/reservation/service/book/BookFeign.java
+++ service/src/main/java/com/reservation/service/book/BookFeign.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(name = "BookFeign", url = "http://localhost:8080")
@@ -14,6 +15,6 @@ public interface BookFeign {
     @GetMapping("/api/books/disponivel/{isbn}/isbn")
     Boolean isDisponivelByIsbn(@PathVariable String isbn) throws EntityNotFoundException;
 
-    @PatchMapping("/books/{isbn}")
+    @PutMapping("/api/books/isbn/{isbn}")
     void patchStatus(@PathVariable("isbn") String isbn, @RequestBody BookPatchRequestDTO request) throws EntityNotFoundException;
 }
